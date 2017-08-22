1be019ead87805a74c7201aa861c1061b62f55ec
Ian de Jesus <ian.dejesus.md@gmail.com>
2017-08-22 16:39:19 +0800

    Added passing_functions.py

---
+++ decorators.py
@@ -0,0 +1,67 @@
+# A decorator is just a function that gets called before another function
+
+import functools  # function tools
+
+
+def my_decorator(f):
+    @functools.wraps(f)
+    def function_that_runs_f():
+        print("Hello!")
+        f()
+        print("After!")
+
+    return function_that_runs_f
+
+
+@my_decorator  # passing my_function to my_decorator
+def my_function():  # replaced by function_that_runs_f
+    print("I'm in the function.")
+
+
+my_function()
+
+
+###
+
+def my_decorator(f):
+    @functools.wraps(f)
+    def function_that_runs_f(*args, **kwargs):
+        print("Hello!")
+        f(*args, **kwargs)
+        print("After!")
+
+    return function_that_runs_f
+
+
+@my_decorator
+def my_function(arg1, arg2):
+    print(arg1 + arg2)
+
+
+my_function(56, 89)
+
+
+###
+
+def decorator_arguments(number):
+    def my_decorator(f):
+        @functools.wraps(f)
+        def function_that_runs_f(*args, **kwargs):
+            print("Hello!")
+            if number == 56:  # checks if parameter is 56
+                print("Not running!")
+            else:
+                f(*args, **kwargs)
+            print("After")
+
+        return function_that_runs_f
+
+    return my_decorator
+
+
+@decorator_arguments(52)
+def my_function(x, y):
+    print(x + y)
+
+
+my_function(5,8)
